18d7f30b986108ecc8194e3dd6a5ab698c6c187c
Bohdan Vasylyk <bogdanvasilik2120@gmail.com>
2023-12-15 19:06:01 +0200

    added popular dishes and functional update

--- routes/foods.js
+++ routes/foods.js
@@ -5,12 +5,16 @@ const {
   getFoodsSeasonal,
   getSearchFoods,
   getFoodByCategory,
+  getSortPopularFoods,
+  updatePopularFoods,
 } = require("../controllers/foods");
 
 // Foods
 router.get("/", getAllFoods);
 router.get("/seasonal", getFoodsSeasonal);
 router.get("/search", getSearchFoods);
+router.get("/popular", getSortPopularFoods);
+router.post("/popular/update", updatePopularFoods);
 router.get("/category/:category", getFoodByCategory);
 router.get("/:foodId", getFoodsById);
 
